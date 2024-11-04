04805768de51641e7599f535afd5d3e160feb209
AdrianReyhan <adrianreyhan03.ar@gmail.com>
2024-11-04 09:00:49 +0700

    push projek awal

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-white rounded shadow p-5 mb-4 mt-4 text-center">
+    <div class="row items-center center">
+        <div class="col-12 col-md-4 col-xl-6 mb-4 mb-md-0">
+            <p class="mb-0 text-center text-lg-start">©<span class="current-year"></span> <a class="text-primary fw-normal"
+                    href="https://themesberg.com" target="_blank"> - Disdukcapil Kota Semarang
+                </a></p>
+        </div>
+    </div>
+</footer>
